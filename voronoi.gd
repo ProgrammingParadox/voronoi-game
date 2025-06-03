9fe271b02de3d1b88e47468f643f46c0dd91afbd
ProgrammingParadox <theparadox31415@gmail.com>
2025-06-03 00:26:26 -0700

    fix player coords in voronoi shader

--- voronoi.gd
+++ voronoi.gd
@@ -27,10 +27,6 @@ func _ready():
 		point_x.append(randf())
 		point_y.append(randf())
 
-		randomize()
-		
-		#var c = [randf(), randf(), randf(), 1.0]
-		
 		var c = color_palette[randi() % color_palette.size()]
 		
 		colors.append(c[0])
@@ -46,14 +42,16 @@ func _ready():
 func _process(delta: float) -> void:
 	var player_position = get_parent().get_node("Player").position
 	
-	var px = player_position.x
-	var py = player_position.y
+	var m = max(screen_size.x, screen_size.y);
 	
-	material.set_shader_parameter("player_x", px / screen_size.x)
-	material.set_shader_parameter("player_y", py / screen_size.y)
+	var px = player_position.x/m
+	var py = player_position.y/m
 	
-	point_x[0] = px/screen_size.x;
-	point_y[0] = py/screen_size.y;
+	material.set_shader_parameter("player_x", px)
+	material.set_shader_parameter("player_y", py)
+	
+	point_x[0] = px;
+	point_y[0] = py;
 	
 	material.set_shader_parameter("p_x", point_x)
 	material.set_shader_parameter("p_y", point_y)
