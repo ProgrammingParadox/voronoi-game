d68f43b6f1f68dd5e4ca5813d95799a7b6aed1dd
Whopies <ianwride021@gmail.com>
2025-06-03 08:38:49 -0700

    added bounce to pointer

--- pointer.gd
+++ pointer.gd
@@ -1,10 +1,11 @@
 extends Label
 
-@onready var thing1 = $"../VBoxContainer/Thing1"
-@onready var thing2 = $"../VBoxContainer/Thing2"
-@onready var thing3 = $"../VBoxContainer/Thing3"
-
+@onready var thing1 = $"../VBoxContainer/Resume"
+@onready var thing2 = $"../VBoxContainer/Settings"
+@onready var thing3 = $"../VBoxContainer/Quit"
+var timer = 0.0
 var curr_selection := 1
+var bounce = 15 * sin(timer*2) #bounce speed and size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = thing1.position
@@ -12,21 +13,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	timer += delta
 	if curr_selection == 1:
-		get_node("../VBoxContainer/Thing1").position.x = 10
-		get_node("../VBoxContainer/Thing2").position.x = 0
-		get_node("../VBoxContainer/Thing3").position.x = 0
-		position = thing1.position
+		get_node("../VBoxContainer/Resume").position.x = 10
+		get_node("../VBoxContainer/Settings").position.x = 0
+		get_node("../VBoxContainer/Quit").position.x = 0
+		position.x = thing1.position.x + 15 * sin(timer*4)
+		position.y = thing1.position.y
 	elif curr_selection == 2:
-		get_node("../VBoxContainer/Thing1").position.x = 0
-		get_node("../VBoxContainer/Thing2").position.x = 10
-		get_node("../VBoxContainer/Thing3").position.x = 0
-		position = thing2.position
+		get_node("../VBoxContainer/Resume").position.x = 0
+		get_node("../VBoxContainer/Settings").position.x = 10
+		get_node("../VBoxContainer/Quit").position.x = 0
+		position.x = thing2.position.x + 15 * sin(timer*4)
+		position.y = thing2.position.y
 	else:
-		get_node("../VBoxContainer/Thing1").position.x = 0
-		get_node("../VBoxContainer/Thing2").position.x = 0
-		get_node("../VBoxContainer/Thing3").position.x = 10
-		position = thing3.position
+		get_node("../VBoxContainer/Resume").position.x = 0
+		get_node("../VBoxContainer/Settings").position.x = 0
+		get_node("../VBoxContainer/Quit").position.x = 10
+		position.x = thing3.position.x + 15 * sin(timer*4)
+		position.y = thing3.position.y
 	if Input.is_action_just_pressed("player_1_down"):
 		curr_selection += 1
 	elif Input.is_action_just_pressed("player_1_up"):
