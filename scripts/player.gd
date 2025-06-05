918340772fcb7300eda9cc8b5c7b6680ba19f05a
Whopies <ianwride021@gmail.com>
2025-06-05 14:52:21 -0700

    made the walls grey when placing

--- scripts/player.gd
+++ scripts/player.gd
@@ -30,6 +30,7 @@ var screen_size
 var is_building_wall = false
 var wall_start_pos = Vector2.ZERO
 var current_wall = null
+var wall_a = 0.2
 
 @onready var parent = get_node("/root/Node2D"); 
 
@@ -57,31 +58,35 @@ func plant():
 	
 	parent.add_point(new_seed);
 
+func wall_state(collision: bool, transperancy: float):
+	var collision_shape = current_wall.get_child(0).get_child(0)
+	var sprite = current_wall.get_child(0).get_child(1)
+	var matter = sprite.get_material()
+	matter.set_shader_parameter("transp", transperancy)
+	collision_shape.disabled = not collision
+
 func start_wall_building():
-	
 	is_building_wall = true
 	wall_start_pos = global_position
-	
 	current_wall = wall_tscn.instantiate()
 	add_sibling(current_wall)
-	var collision_shape = current_wall.get_child(0).get_child(0)
-	collision_shape.disabled = true
+	wall_state(false, 0.3)
 	current_wall.global_position = wall_start_pos
 
 func finish_wall_building():
-	var start_pos = current_wall.global_position
-	var distance = sqrt(((global_position.y - current_wall.global_position.y)**2)+((global_position.x - current_wall.global_position.x)**2))/6
-	var angle = start_pos.angle_to_point(global_position)
-	var collision_shape = current_wall.get_child(0).get_child(0)
-	current_wall.rotation = angle
-	current_wall.scale.x = distance
-	collision_shape.disabled = false
+	wall_state(true, 1.0)
 	is_building_wall = false
 	current_wall = null
 	wall_start_pos = Vector2.ZERO
 	
+func wall_update():
+	var start_pos = current_wall.global_position
+	var distance = sqrt(((global_position.y - current_wall.global_position.y)**2)+((global_position.x - current_wall.global_position.x)**2))/6
+	var angle = start_pos.angle_to_point(global_position)
+	current_wall.rotation = angle
+	current_wall.scale.x = distance
 	
-	
+
 
 func dash(delta):
 	var entities = get_tree().get_nodes_in_group("entity");
@@ -170,6 +175,9 @@ func _physics_process(delta: float) -> void:
 			start_wall_building()
 		else:
 			finish_wall_building()
+	
+	if is_building_wall:
+		wall_update()
 	
 	if Input.is_action_just_pressed(controls_plant) && energy > 20:
 		plant()
