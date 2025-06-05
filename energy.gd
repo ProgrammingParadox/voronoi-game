7a3efa067e69e7501c0a6274cdb2c4498cafc3b1
Whopies <ianwride021@gmail.com>
2025-06-04 21:13:58 -0700

    added energy bars

--- energy.gd
+++ energy.gd
@@ -2,19 +2,19 @@ extends Control
 
 @export var player_ref : Node
 @onready var progress_bar = $ProgressBar
-@onready var energy_num = $ProgressBar/EnergyDisplay
+
+@export var energy_speed := 10.0
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player_ref.energy = 0.0
-	energy_num.text = "0"
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress_bar.value += 0.2
-	player_ref.energy = floor(progress_bar.value / 10)
-	energy_num.text = str(int(player_ref.energy))
+	player_ref.energy += energy_speed * delta
+	progress_bar.value = player_ref.energy
 	
 	
 	
--- progress_bar.gd
+++ progress_bar.gd
@@ -1,11 +1,16 @@
 extends ProgressBar
 
+@onready var screen_size = DisplayServer.screen_get_size()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	value += 0.1
+	if get_parent().is_in_group("1st"):
+		global_position = Vector2(0.0,43.0)
+	else:
+		global_position = Vector2(screen_size.x - size.x, 43.0)
