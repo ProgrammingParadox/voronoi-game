dfb2c575d8ee71df209b33f6e94b5d0c0a9cef88
ProgrammingParadox <theparadox31415@gmail.com>
2025-06-05 01:32:13 -0700

    timer display fixed

--- scenes/match_timer.gd
+++ scenes/match_timer.gd
@@ -4,17 +4,22 @@ var display_num : String
 var minutes : int
 var seconds : int
 @onready var timer = $Timer
-@onready var screen_size = DisplayServer.screen_get_size()
+@onready var screen_size = get_viewport_rect().size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.start()
 	seconds = Global.game_time
-	position = Vector2((screen_size.x/2.0)-(size.x/2.0), 43)
+	
+	screen_size = get_viewport_rect().size
+	position.x = (screen_size.x / 2.0) - (size.x / 2.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	text = sec_to_cool(Global.game_time)
+	
+	screen_size = get_viewport_rect().size
+	position.x = (screen_size.x / 2.0) - (size.x / 2.0)
 
 func _on_timer_timeout() -> void:
 	Global.game_time -= 1
