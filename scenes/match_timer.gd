b87763253714dbcecbdf0ee1f4b80ccb49fc3472
Whopies <ianwride021@gmail.com>
2025-06-04 21:43:02 -0700

    made game timer better

--- scenes/match_timer.gd
+++ scenes/match_timer.gd
@@ -1,11 +1,25 @@
 extends Label
 
-
+var display_num : String
+var minutes : int
+var seconds : int
+@onready var timer = $Timer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	timer.start()
+	seconds = Global.game_time
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	text = sec_to_cool(Global.game_time)
+
+func _on_timer_timeout() -> void:
+	Global.game_time -= 1
+
+func sec_to_cool(seconds):
+	var minutes := 0
+	while seconds > 59:
+		seconds -= 60
+		minutes += 1
+	return str(int(minutes))+":"+str(int(seconds))
